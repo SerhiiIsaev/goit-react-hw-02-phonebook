e1bfcc78f928ef343b7edecc7c1f05990c4e0b5e
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-09-19 21:06:28 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Component } from "react";
 import { nanoid } from 'nanoid';
-
 import { ContactForm } from "./ContactForm/ContactForm";
 import { Filter } from "./Filter/Filter";
 import { ContactList } from "./ContactList/ContactList";
